e1ba50247754d727e3b7a6381410c58fddd63226
Mauro <mauroandres1246@gmail.com>
2018-05-13 22:30:27 -0600

    Se agrego el servicio post a la aplicacion

--- src/app/entries/entry/entry-comment-component-form.ts
+++ src/app/entries/entry/entry-comment-component-form.ts
@@ -1,5 +1,6 @@
-import { Component,EventEmitter,Output,ViewChild } from "@angular/core";
+import { Component,EventEmitter,Input,Output,ViewChild } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { EntryService } from "../shared/entry.service";
 
 @Component({
     selector: 'app-entry-comment-form',
@@ -8,11 +9,17 @@ import { NgForm } from "@angular/forms";
 export class EntryCommentFormComponent{
     name :string;
     comment: string;
+    @Input() entryId:number;
     @Output() onCommentAdded = new EventEmitter<{name:string;comment:string;}>();
     @ViewChild('commentForm') commentForm:NgForm;
+    constructor(private entryService:EntryService){}
     onSubmit(commentForm:NgForm){
         let comment = {name:this.name,comment:this.comment};
-        this.onCommentAdded.emit(comment);
-        this.commentForm.reset();
+        this.entryService.addEntries(this.entryId,comment)
+        .then(()=>{
+            this.onCommentAdded.emit(comment);
+            this.commentForm.reset();
+        });
+        
     }
 }
